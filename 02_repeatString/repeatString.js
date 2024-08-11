46d14da7fb5c5c0ebecfefe2e7394a8e70dfc29b
Phillip Martinez <phillip.martinez210@gmail.com>
2024-08-10 21:26:20 -0500

    Wrote and tested my solution. All tests passed.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeatTimes) {
+    let repeatedString = "";
+    if (repeatTimes < 0) {
+        return "ERROR";
+    } else {
+        for (let i = 0; i < repeatTimes; i++) {
+            repeatedString += string;
+        }
+        return repeatedString;
+    }
 };
 
 // Do not edit below this line
