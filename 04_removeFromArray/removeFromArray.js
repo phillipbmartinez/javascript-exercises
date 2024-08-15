a04f60e476e51b62016fd52ea427358b4c778758
Phillip Martinez <phillip.martinez210@gmail.com>
2024-08-15 15:07:08 -0500

    Exercise 04 completed.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,17 +1,26 @@
-// const removeFromArray = function() {
-// };
-
-function removeFromArray(array, removeItem) {
+const removeFromArray = function (array, ...args) {
+    // create a new empty array
     const newArray = [];
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] !== removeItem) {
-            newArray.push(array[i]);
-        }
-    }
-    console.log(`array: ${array}`);
-    console.log(`emptyArray: ${newArray}`);
+    // use forEach to go through the array
+    array.forEach((item) => {
+      // push every element into the new array
+      // UNLESS it is included in the function arguments
+      // so we create a new array with every item, except those that should be removed
+      if (!args.includes(item)) {
+        newArray.push(item);
+      }
+    });
+    // and return that array
     return newArray;
-}
+  };
+  
+  // A simpler, but more advanced way to do it is to use the 'filter' function,
+  // which basically does what we did with the forEach above.
+  
+  // const removeFromArray = function(array, ...args) {
+  //   return array.filter(val => !args.includes(val))
+  // }
+  //
 
 // Do not edit below this line
 module.exports = removeFromArray;
