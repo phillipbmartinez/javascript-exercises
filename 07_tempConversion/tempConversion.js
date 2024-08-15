45a74dc2bd56b6d4de352ee8ab2eb0a4cc2a765e
Phillip Martinez <phillip.martinez210@gmail.com>
2024-08-15 15:17:41 -0500

    Exercise 07 passed and completed.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (degrees) {
+  degreesCelsius = (degrees - 32) * (5/9);
+  console.log(degreesCelsius);
+  roundedDegreesCelsius = Math.round(degreesCelsius * 10) / 10;
+  return roundedDegreesCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (degrees) {
+  degreesFahrenheit = degrees * (9/5) + 32;
+  console.log(degreesFahrenheit);
+  roundedDegreesFahrenheit = Math.round(degreesFahrenheit * 10) / 10;
+  return roundedDegreesFahrenheit;
 };
 
 // Do not edit below this line
