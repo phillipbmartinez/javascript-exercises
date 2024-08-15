6cf0c466250ae3dfee3faf0c89a26c278d3adc15
Phillip Martinez <phillip.martinez210@gmail.com>
2024-08-15 14:35:47 -0500

    For exercise 04, got the first test to pass.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-};
+// const removeFromArray = function() {
+// };
+
+function removeFromArray(array, removeItem) {
+    const newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] !== removeItem) {
+            newArray.push(array[i]);
+        }
+    }
+    console.log(`array: ${array}`);
+    console.log(`emptyArray: ${newArray}`);
+    return newArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
