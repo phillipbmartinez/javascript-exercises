3d5f31b9f371c10f01e395927507c922857a006e
Phillip Martinez <phillip.martinez210@gmail.com>
2024-08-15 15:24:40 -0500

    Completed exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+    newArray = [];
+    for (let i = 0; i < books.length; i++) {
+        newArray.push(books[i].title);
+        console.log(newArray);
+    }
+    return newArray;
 };
 
 // Do not edit below this line
